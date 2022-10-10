b1f629467fe8a6146baaafd6430087b6478108cc
satinder <satinder1624@gmail.com>
2022-10-10 18:36:27 -0400

    Fix bug

--- pages/watchlist/index.js
+++ pages/watchlist/index.js
@@ -2,7 +2,7 @@ import { useSession } from "next-auth/react";
 import SignINIMDB from "../../components/signInIMDB/signINIMDB";
 import ShowingWatchListMovies from "./showingWatchListMovies";
 
-export default function watchlist() {
+export default function WatchList() {
   const { data: session } = useSession();
 
   return session ? <ShowingWatchListMovies /> : <SignINIMDB />;
